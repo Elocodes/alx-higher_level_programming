5a8cf144032e18ac73a827f861780502c768791c
Elocodes <elochukwuodoeze@gmail.com>
2023-03-08 09:36:23 -0800

    py functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,19 +6,22 @@ last_digit = str(number)[-1]
 digit2int = int(last_digit)
 neg_last_digit = -1 * digit2int
 
-if digit2int > 5:
-    if number > 0:
-        print(f"Last digit of {number} is {digit2int} and \
-is greater than 5")
-    elif number < 0:
-        print(f"Last digit of {number} is {neg_last_digit} and \
-is less than 6 and not 0")
-elif digit2int in range(1, 6):
-    if number > 0:
-        print(f"Last digit of {number} is {digit2int} and \
-is less than 6 and not 0")
-    elif number < 0:
-        print(f"Last digit of {number} is {neg_last_digit} and \
-is less than 6 and not 0")
-else:
-    print(f"Last digit of {number} is {digit2int} and is 0")
+try:
+    if digit2int > 5:
+        if number > 0:
+            print(f"Last digit of {number} is {digit2int} and \
+    is greater than 5")
+        elif number < 0:
+            print(f"Last digit of {number} is {neg_last_digit} and \
+    is less than 6 and not 0")
+    elif digit2int in range(1, 6):
+        if number > 0:
+            print(f"Last digit of {number} is {digit2int} and \
+    is less than 6 and not 0")
+        elif number < 0:
+            print(f"Last digit of {number} is {neg_last_digit} and \
+    is less than 6 and not 0")
+    else:
+        print(f"Last digit of {number} is {digit2int} and is 0")
+except Valuerror as err:
+    print('value error: ' + str(err))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 space = " "
-"""last_num = 99"""
 
-for each_num in range(0, 99):
-    if each_num < 10:
-        num2str = str(each_num)
-        str_zero = '0'
-        to_print = str_zero + num2str
+for each_num in range(0, 100):
+    if each_num == 99:
+        print("99")
     else:
-        to_print = each_num
-    print("{}, {}".format(to_print, space), end='')
-print("99")
+        if each_num < 10:
+            num2str = str(each_num)
+            str_zero = '0'
+            to_print = str_zero + num2str
+        else:
+            to_print = each_num
+        print("{}, {}".format(to_print, space), end='')
