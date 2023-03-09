d8b18c2cc952f3ce3d2a3797c8b54b52019efb58
Elocodes <elochukwuodoeze@gmail.com>
2023-03-08 17:11:52 -0800

    py function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    """function prints a string in uppercase"""
+"""def uppercase(str):
+    function prints a string in uppercase
     for each_letter in str:
         each_letter = ord(each_letter)
         if each_letter in range(97, 123):
             each_letter -= 32
         to_upper = chr(each_letter)
         print("{}".format(to_upper), end='')
+    print("")"""
+
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        i = ord(i)
+        if i >= 97 and i <= 122:
+            i -= 32
+        i = chr(i)
+        print("{}".format(i), end='')
     print("")
