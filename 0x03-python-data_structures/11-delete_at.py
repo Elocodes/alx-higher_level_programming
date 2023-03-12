968146b90c1dd1e3124d56537cae0dd9cd88c9d6
Elocodes <elochukwuodoeze@gmail.com>
2023-03-12 10:36:50 -0700

    lists and tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,11 +2,11 @@
 
 def delete_at(my_list=[], idx=0):
     """function deletes the item in my list whose index is given"""
-
+    
     if idx < 0 or idx > len(my_list):
         return (my_list)
     else:
         for index, each_num in enumerate(my_list):
             if idx == index:
                 del my_list[index]
-                return (my_list)
+        return (my_list)
