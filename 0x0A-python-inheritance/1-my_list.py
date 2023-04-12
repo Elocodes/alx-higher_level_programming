ab123da85b84d36d0e39c73f984a2e1d0d63ff40
Elocodes <elochukwuodoeze@gmail.com>
2023-04-11 19:48:44 -0700

    bug fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,8 +12,8 @@ class MyList(list):
 
     Arg:
         param: list is an inbuilt python class. other classes can
-               inherit from it. 
-    """
+        inherit from it.
+        """
     def __init__(self):
         """function initializes attributes.
 
@@ -21,7 +21,7 @@ class MyList(list):
         This will allow our subclass use its attributes.
         """
         list.__init__([])
-    
+
     def print_sorted(self):
         """function prints the contents of a list in ascending order.
         in this case, all elements of list are of type int.
