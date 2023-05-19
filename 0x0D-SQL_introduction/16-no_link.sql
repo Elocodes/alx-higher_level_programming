2044a025e1e3bdfa48a01ef7ef19d27a4fdbdeb8
Elocodes <elochukwuodoeze@gmail.com>
2023-05-19 14:27:07 -0700

    added requirement for empty name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,6 +2,7 @@
 -- Dont list rows that do not have a name value
 -- output is to be ordered by score in descending order
 
-SELECT ALL score, name
+SELECT score, name
 FROM second_table
+WHERE name != ''
 ORDER BY score DESC;
