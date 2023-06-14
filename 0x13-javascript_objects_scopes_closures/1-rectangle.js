b590d6761c3c48a38e6a88f08e005717cb8c009e
Elocodes <elochukwuodoeze@gmail.com>
2023-06-14 18:58:58 +0000

    rect init func

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// initializing width and height of rectangle
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
